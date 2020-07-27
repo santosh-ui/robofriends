d1e03fc0d6469cba7f2f5e3f84eb009d64c09a3a
santosh-ui <16r91a0547tkrec@gmail.com>
2020-07-27 10:48:59 +0530

    used jsonplaceholder for robots array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import {robots} from './robots';
 import './App.css'
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			Searchfield: ''
 		}
 	}
@@ -17,17 +16,28 @@ class App extends Component {
 		this.setState({Searchfield : event.target.value})
 	}
 
+	componentDidMount() {
+		fetch('http://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ robots: users }));
+	}
+
 	render() {
 		const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.Searchfield.toLowerCase())
 		})
-		return (
-			<div className="tc">
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots}/>
-			</div>
-		);
+		if(this.state.robots.length === 0) {
+			return <h1 className='tc'>Loading</h1>
+		}else{
+			return (
+				<div className="tc">
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<CardList robots={filteredRobots}/>
+				</div>
+			);
+		}
+		
 	}
 	
 }
